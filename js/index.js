f3863817f8eba33e6053f44fe8384e0ab86bdd2d
Devansh <devanshkapri19@gmail.com>
2021-09-26 15:57:55 +0530

    samwad-21 edit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$(document).ready(() => {
+  $(window).on("scroll", () => {
+    console.log($(this).scrollTop());
+    console.log(window.innerHeight);
+    if ($(this).scrollTop() > window.innerHeight / 2) {
+      console.log("hello");
+      $("nav.navbar").addClass("transparent");
+    } else {
+      $("nav.navbar").removeClass("transparent");
+    }
+  });
+});
